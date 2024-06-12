006c49f50cb702709ba33e439c50fa16ce918282
KinkPolice <mr.silerfox@yandex.ru>
2024-06-12 13:33:21 +0500

    html/css calc. maybe change to generate grid w/js->push objects from array?

--- javascript.js
+++ javascript.js
@@ -1,25 +1,26 @@
+const inputs = document.querySelector('.inputs')
+const divide = document.querySelector('#divide')
+const multiply = document.querySelector('#multiply')
+const subtr = document.querySelector('#subtraction')
+const add = document.querySelector('#add')
 
-
-
-
-
-
-
+    
+    
 let result
 
 
+add.addEventListener('click', function(){
+    return result = a + b
+})
 
-function add(a,b){
-    result = a + b;
-}
-function subtract(a, b){
-    result = a - b;
-}
+subtr.addEventListener('click', function(){
+    return result = a - b
+})
 
-function multiply(a, b){
-    result = a * b;
-}
+multiply.addEventListener('click', function(){
+    return result = a * b
+})
 
-function divide(a, b){
-    result = a/b;
-}
+divide.addEventListener('click', function(){
+    return result = a/b
+})
