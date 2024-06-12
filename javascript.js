ae405ad669b0b81e0919fd42ef9bf89660ad14c9
KinkPolice <mr.silerfox@yandex.ru>
2024-06-12 12:34:48 +0500

    starting out

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+let result
+
+
+
+function add(a,b){
+    result = a + b;
+}
+function subtract(a, b){
+    result = a - b;
+}
+
+function multiply(a, b){
+    result = a * b;
+}
+
+function divide(a, b){
+    result = a/b;
+}
